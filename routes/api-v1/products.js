00e03e16ef512c63bb65c032d1073fcbb56756db
Alberto Gonzalez <al286290@uji.es>
2016-02-11 02:55:56 +0100

    Product CRUD operations implemented

--- routes/api-v1/products.js
+++ routes/api-v1/products.js
@@ -26,7 +26,6 @@ module.exports = function() {
     // Check if we are offering the product that the client is looking for
     router.use('/:productType', function(req, res, next) {
         if (_.contains(defaults.allLocalProducts, req.params.productType)) {
-            req.ERMES.ProductType = sequelize.import(path.resolve('./models/' + _.singularize(req.params.productType)));
             next();
         } else {
             res.type('text/plain');
@@ -40,7 +39,7 @@ module.exports = function() {
         var productType = _.singularize(req.params.productType);
         var parcelIds = _.map(req.body[productType].parcels, (parcel) => parcel.toLowerCase());
         var receivedProduct = _.omit(req.body[productType], ['parcels']);
-        var ProductType = req.ERMES.ProductType;
+        var ProductType = sequelize.import(path.resolve('./models/' + _.singularize(req.params.productType)));
 
         sequelize.transaction((t) => {
            var outterProductProperties = _.pick(receivedProduct, ['uploadDate', 'shared']);
@@ -55,14 +54,14 @@ module.exports = function() {
                   if (user.type === 'owner') {
                       var usersIds = [user.userId];
 
-                      return checkAndAddProduct(parcelIds, usersIds, product, t).then(() => {
+                      return checkAndAddProductToParcel(parcelIds, usersIds, product, t).then(() => {
                           return [product, innerProduct];
                       });
                   } else {
                       return user.getOwners({transaction: t}).then((owners) => {
                           var usersIds = _.map(owners, (owner) => owner.userId);
 
-                          return checkAndAddProduct(parcelIds, usersIds, product, t).then(() => {
+                          return checkAndAddProductToParcel(parcelIds, usersIds, product, t).then(() => {
                               return [product, innerProduct];
                           });
                       });
@@ -70,7 +69,8 @@ module.exports = function() {
                });
            });
         }).then((result) => {
-            var product = result[0].get({plain: true});
+            buildProduct(res, result, productType);
+            /*var product = result[0].get({plain: true});
             var innerProduct = result[1].get({plain: true});
 
             // Combine both general and specific product
@@ -80,14 +80,14 @@ module.exports = function() {
             // Mixed response
             var response = {};
             response[productType] = product;
-            res.status(201).json(response);
+            res.status(201).json(response);*/
         }).catch((ex) => {
             console.error('ERROR CREATING PRODUCT: ' + ex);
             res.status(200).json({errors: {name: [ex.name, ex.message]}});
         });
     });
 
-    function checkAndAddProduct(parcelIds, userIds, product, t) {
+    function checkAndAddProductToParcel(parcelIds, userIds, product, t) {
         return Parcel.findAll({where: {parcelId: {$in: parcelIds}},
             includes: [{model: User, as: 'owners', where: {userId: {$in: userIds}}}], transaction: t}).then((parcels) => {
             if (parcels.length !== parcelIds.length) {
@@ -98,30 +98,145 @@ module.exports = function() {
         });
     }
 
-    router.use('/:productType/:id', function(req, res, next){
+    router.use('/:productType/:productId', function(req, res, next){
+        var productId = req.params.productId;
 
-
-        if(!req.params.id.match("^[0-9]+$")){
+        if(!productId.match("^[0-9]+$")){
             res.status(404).send({errors: {name: ['NOT FOUND']}});
         }
         else{
-            // TODO Look for the product and put it on the request
-            next();
+            Product.findOne({where: {productId: productId}}).then((product) => {
+                if (!product) {
+                    throw new Error('Not found');
+                }
+                req.ERMES.product = product;
+                next();
+            }).catch((ex) => {
+                console.error('PRODUCT NOT FOUND: ' + productId);
+                res.status(404).json({errors: {name: [ex.name, ex.message]}});
+            });
         }
 
     });
 
     router.get('/:productType/:id', function(req, res) {
+        var user = req.ERMES.user;
+        var product = req.ERMES.product;
+
+        new Promise((resolve, reject) => {
+            if (user.type === 'owner') {
+                user.getCollaborators().then((collaborators) => {
+                    var userIds = _.map(collaborators, (collaborator) => collaborator.userId);
+                    userIds.push(user.userId);
+                    if (!_.contains(userIds, product.userId)) {
+                        reject(new Error('You do not have access to this product'));
+                    } else {
+                        product.getInnerProduct().then((innerProduct) => {
+                            resolve([product, innerProduct]);
+                        });
+                    }
+                });
+            } else {
+                user.getOwners().then((owners) => {
+                    var userIds = _.map(owners, (owner) => owner.userId);
+                    userIds.push(user.userId);
+                    if (!_.contains(userIds, product.userId)) {
+                        reject(new Error('You do not have access to this product'));
+                    } else {
+                        product.getInnerProduct().then((innerProduct) => {
+                            resolve([product, innerProduct]);
+                        });
+                    }
+                });
+            }
+        }).then((result) => {
+            buildProduct(res, result, product.type);
+        }).catch((ex) => {
+            console.error('FORBIDDEN: ' + product.type + ' ' + product.productId);
+            res.status(401).json({errors: {name: [ex.name, ex.message]}});
+        });
 
     });
 
     router.put('/:productType/:id', function(req, res) {
+        var user = req.ERMES.user;
+        var product = req.ERMES.product;
 
+
+        var updateGeneral = _.pick(req.body[product.type], ['shared']);
+        var updateSpecific = _.omit(req.body[product.type], ['productId', 'createdAt', 'updatedAt']);
+
+        sequelize.transaction((t) => {
+            if (user.userId !== product.userId) {
+                throw new Error('You do not have permission to manage this product');
+            } else {
+                return product.getInnerProduct({transaction: t}).then((innerProduct) => {
+                    return product.update(updateGeneral, {transaction: t}).then(() => {
+                        return innerProduct.update(updateSpecific, {transaction: t}).then(() => {
+                            return [product, innerProduct];
+                        });
+                    });
+
+                });
+            }
+        }).then((result) => {
+            buildProduct(res, result, product.type);
+        }).catch((ex) => {
+            console.error('FORBIDDEN: ' + product.type + ' ' + product.productId);
+            res.status(403).json({errors: {name: [ex.name, ex.message]}});
+        });
     });
 
     router.delete('/:productType/:id', function(req, res) {
+        var user = req.ERMES.user;
+        var product = req.ERMES.product;
 
+        sequelize.transaction((t) => {
+           if (user.userId !== product.userId) {
+               throw new Error('You do not have permission to manage this product');
+           } else {
+               return product.getInnerProduct({transaction: t}).then((innerProduct) => {
+                   var productPlain = product.get({plain: true});
+                   var innerProductPlain = innerProduct.get({plain: true});
+
+                   // Combine both general and specific product
+                   _.extend(productPlain, innerProductPlain);
+                   productPlain = _.omit(productPlain, ommitedProductFields);
+
+                   // Mixed response
+                   var response = {};
+                   response[product.type] = productPlain;
+
+                   // Remove from the system
+
+                   return innerProduct.destroy({transaction: t}).then(() => {
+                       return product.destroy({transaction: t}).then(() => {
+                           return [product, innerProduct];
+                       });
+                   });
+               });
+           }
+        }).then((result) => {
+            buildProduct(res, result, product.type);
+        }).catch((ex) => {
+            console.error('FORBIDDEN: ' + product.type + ' ' + product.productId);
+            res.status(403).json({errors: {name: [ex.name, ex.message]}});
+        });
     });
+
+    function buildProduct(res, result, productType) {
+        var product = result[0].get({plain: true});
+        var innerProduct = result[1].get({plain: true});
+
+        // Combine both general and specific product
+        _.extend(product, innerProduct);
+        product = _.omit(product, ommitedProductFields);
+
+        // Mixed response
+        var response = {};
+        response[productType] = product;
+        res.status(201).json(response);
+    }
 
     /*router.use('/:product/:id', function(req, res, next){
 
